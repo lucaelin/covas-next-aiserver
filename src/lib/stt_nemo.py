3c1634e386c4041bf44619f6ceb7eaa39ed539e4
Luca Haneklau <git@luca.lsys.ac>
2025-05-08 17:07:03 +0200

    feat: add STT parakeet-tdt-0.6b-v2

--- src/lib/stt_nemo.py
+++ src/lib/stt_nemo.py
@@ -5,7 +5,11 @@ import samplerate
 import soundfile as sf
 from nemo.collections.asr.models import EncDecMultiTaskModel
 from typing import Literal
+
+from transformers.models.opt.modeling_opt import OPTSdpaAttention
+
 stt_model_names = [
+    "nvidia/parakeet-tdt-0.6b-v2",
     "nvidia/canary-180m-flash",
     "nvidia/canary-1b-flash",
 ]
@@ -14,22 +18,30 @@ stt_model_names = [
 def init_stt(model_name="nvidia/canary-180m-flash"):
 
     # load model
-    canary_model = EncDecMultiTaskModel.from_pretrained('nvidia/canary-180m-flash')
-    # update decode params
-    decode_cfg = canary_model.cfg.decoding
-    decode_cfg.beam.beam_size = 1
-    canary_model.change_decoding_strategy(decode_cfg)
-    
-    def transcribe(audio: ndarray, language: Literal['en','de','es','fr']):
-        output = canary_model.transcribe(
-            audio,
-            batch_size=1,  # batch size to run the inference with
-            pnc='yes',        # generate output with Punctuation and Capitalization
-            source_lang=language,
-            target_lang=language,
-        )
+    nemo_model = EncDecMultiTaskModel.from_pretrained(model_name)
+    if model_name in ["nvidia/canary-1b-flash", "nvidia/canary-180m-flash"]:
+        # update decode params
+        decode_cfg = nemo_model.cfg.decoding
+        decode_cfg.beam.beam_size = 1
+        nemo_model.change_decoding_strategy(decode_cfg)
+
+    def transcribe(audio: ndarray, language: Literal["en", "de", "es", "fr"]):
+        if model_name in ["nvidia/canary-1b-flash", "nvidia/canary-180m-flash"]:
+            opts = {
+                "audio": audio,
+                "batch_size": 1,  # batch size to run the inference with
+                "pnc": "yes",  # generate output with Punctuation and Capitalization
+                "source_lang": language,
+                "target_lang": language,
+            }
+        if model_name == "nvidia/parakeet-tdt-0.6b-v2":
+            opts = {
+                "audio": audio,
+                "batch_size": 1,  # batch size to run the inference with
+            }
+        output = nemo_model.transcribe(**opts)
         return output
-    
+
     return transcribe
 
 
@@ -47,7 +59,7 @@ def stt(transcribe, wav: bytes, language="en-US"):
 
     start = time.time()
 
-    output = transcribe(audio, language=language.split('-')[0])
+    output = transcribe(audio, language=language.split("-")[0])
     print(output[0])
 
     end = time.time()
